a5ffcc45bda393e7d81ac9f5ecb5d36315829814
javier ponte <jeponteg@gmail.com>
2023-09-09 00:38:15 -0300

    feat: :wrench: Tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "../src/main.css"
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="text-lg font-medium text-rose-500" > hola mundo</div>
       </div>
 
   );
